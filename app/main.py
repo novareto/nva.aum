f69f4c9c289b4f439b2228c35f4f453656cba018
lwalther <lwalther@novareto.de>
2020-12-06 19:50:35 +0100

    Erste Versuche mit Application-Key

--- app/main.py
+++ app/main.py
@@ -4,8 +4,22 @@
 
 from .models import AUMSingle, AUMEnvelope, AUMResponse
 from .services import SiguvServices
-from fastapi import FastAPI
-from fastapi import HTTPException
+
+from fastapi import Security, Depends, FastAPI, HTTPException
+from fastapi.security.api_key import APIKeyQuery, APIKeyCookie, APIKeyHeader, APIKey
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.openapi.utils import get_openapi
+
+from starlette.status import HTTP_403_FORBIDDEN
+from starlette.responses import RedirectResponse, JSONResponse
+
+API_KEY = "1234567asdfgh"
+API_KEY_NAME = "access_token"
+COOKIE_DOMAIN = "localtest.me"
+
+api_key_query = APIKeyQuery(name=API_KEY_NAME, auto_error=False)
+api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
+api_key_cookie = APIKeyCookie(name=API_KEY_NAME, auto_error=False)
 
 tags_metadata = [
     {
@@ -24,17 +38,42 @@ app = FastAPI(
     description="OpenApi für SIGUV-Online-Dienste",
     version="0.9btn (better than nothing)",
     openapi_tags = tags_metadata,
+    docs_url=None,
+    redoc_url=None,
+    openapi_url=None
 )
+
 services = SiguvServices()
+
+async def get_api_key(
+    api_key_query: str = Security(api_key_query),
+    api_key_header: str = Security(api_key_header),
+    api_key_cookie: str = Security(api_key_cookie)):
+
+    if api_key_query == API_KEY:
+        return api_key_query
+    elif api_key_header == API_KEY:
+        return api_key_header
+    elif api_key_cookie == API_KEY:
+        return api_key_cookie
+    else:
+        raise HTTPException(
+            status_code=HTTP_403_FORBIDDEN, detail="Could not validate credentials"
+        )
+
 
 @app.get("/", tags=["Standard"])
 def read_root():
     """Willkommmen"""
     return{"message":"Herzlich Willkommen im SIGUV-Portal für die Veröffentlichung von OpenAPI-Schnittstellen"}
 
+@app.get("/secure_endpoint", tags=["Standard"])
+async def get_open_api_endpoint(api_key: APIKey = Depends(get_api_key)):
+    response = "How cool is this?"
+    return response
 
 @app.post("/{api_version}/single_aum", response_model=AUMResponse, tags=["AUM"])
-def send_single_aum(api_version:str, data:AUMSingle):
+def send_single_aum(api_version:str, data:AUMSingle, api_key: APIKey = Depends(get_api_key)):
     """
     # Übermittlung einer einzelnen Ärztlichen Unfallmeldung
 
